5c0871fc32546eaa100cf5a568637f115634e865
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-03-26 14:35:24 +0100

    rename de toute les fichiers

---
+++ ee/FonctionsDB.php
@@ -0,0 +1,25 @@
+<?php
+    require('./DbConfig.php');
+    require('./GenerateToken.php');
+
+    function searchMedecin($login, $mdp){
+        $DbConfig = DbConfig::getDbConfig();
+        try {
+            $req = $DbConfig->getPDO()->prepare('SELECT * FROM secretaire WHERE login = :login AND mdp = :mdp');
+            $req->execute(array(
+                'login' => $login,
+                'mdp' => $mdp,
+            ));
+            $result = $req->fetch(PDO::FETCH_ASSOC);
+            if ($result !== false) {
+                return true;
+            } else {
+                return $result;
+            }
+        } catch (Exception $pe) {
+            deliver_response(400, "Erreur de connexion : " . $pe->getMessage(), null);
+        }
+    
+    }
+
+?>
---
+++ ee/Index.php
@@ -0,0 +1,43 @@
+<?php 
+    require('./FonctionsDB.php');
+    require('./GenerateToken.php');
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    try{  
+        if (searchMedecin($login, $mdp)) {
+            $login = $data['login'];
+            $mdp = $data['mdp'];
+            $headers = [ 'alg' => 'HS256', 'typ' => 'JWT', ];
+            $payload = ['login' => $login,'mdp' => $mdp,'exp' => time() + 3600];
+            $secret = 'secret';
+            $token = generate_jwt($headers, $payload, $secret);
+            setcookie("usertoken", $token);
+            deliver_response(200, "Authentification réussie", $token);
+        } else {
+            deliver_response(404, "Authentification échouée", null);
+        }
+    } catch (Exception $e) {    
+        echo 'Erreur : ' . $e->getMessage();
+    }
+    }
+
+    if($_SERVER['REQUEST_METHOD'] === 'GET'){
+        try{
+            if($_COOKIE['usertoken']){
+                $token = $_COOKIE['usertoken'];
+                $secret = 'secret';
+                if(is_jwt_valid($token, $secret)){
+                    deliver_response(200, "Authentification réussie", $token);
+                } else {
+                    deliver_response(404, "Authentification échouée", null);
+                }
+            } else {
+                deliver_response(404, "Authentification échouée", null);
+            }
+        } catch (Exception $e) {    
+            echo 'Erreur : ' . $e->getMessage();
+        }
+    }
+?>
