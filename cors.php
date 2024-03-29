2500a23163acbfb0a588f3c876bd40f6e1b4e8e1
EnzoTlz <enzo31del@gmail.com>
2024-03-29 23:28:33 +0100

    test cors https

--- cors.php
+++ cors.php
@@ -1,13 +1,14 @@
 <?php
     
-    header("Access-Control-Allow-Origin: https://gestionmedicalfront.alwaysdata.net/");
+    header("Access-Control-Allow-Origin: https://gestionmedicalfront.alwaysdata.net");
     header('Access-Control-Allow-Credentials: true');
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH");
-    header("Access-Control-Allow-Headers: Content-Type");
-
+    header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Assurez-vous d'inclure tous les en-têtes nécessaires
+    
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200);
         exit();
     }
+    
 
 ?>
